19adc0f28ae001d907f20ac751ba336990368593
yoiki <ishihara_yoshiaki@cyberagent.co.jp>
2022-12-03 18:20:12 +0900

    refactor test code

--- pkg/model/money_test.go
+++ pkg/model/money_test.go
@@ -6,31 +6,34 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestMoney_NewDollar(t *testing.T) {
+	t.Parallel()
+	t.Run("NewDollar", func(t *testing.T) {
+		assert.Equal(t, &Money{amount: 5, monetaryUnit: "USD"}, NewDollar(5))
+	})
+	t.Run("NewFranc", func(t *testing.T) {
+		assert.Equal(t, &Money{amount: 5, monetaryUnit: "CHF"}, NewFranc(5))
+	})
+}
+
 func TestMoney_Getter(t *testing.T) {
 	t.Parallel()
 	t.Run("Amount", func(t *testing.T) {
 		assert.Equal(t, 10, NewDollar(10).Amount())
-		assert.Equal(t, 10, NewFranc(10).Amount())
-		assert.NotEqual(t, 10, NewFranc(5).Amount())
+		assert.NotEqual(t, 10, NewDollar(5).Amount())
 	})
 	t.Run("MonetaryUnit", func(t *testing.T) {
 		assert.Equal(t, "USD", NewDollar(10).MonetaryUnit())
-		assert.Equal(t, "CHF", NewFranc(10).MonetaryUnit())
-		assert.NotEqual(t, "USD", NewFranc(10).MonetaryUnit())
+		assert.NotEqual(t, "CHF", NewDollar(10).MonetaryUnit())
 	})
 }
 
 func TestMoney_Multiplication(t *testing.T) {
 	t.Parallel()
-	t.Run("Multiplication: Dollar", func(t *testing.T) {
+	t.Run("Multiplication", func(t *testing.T) {
 		d := NewDollar(5)
 		assert.Equal(t, NewDollar(10), d.Times(2))
 		assert.Equal(t, NewDollar(15), d.Times(3))
-	})
-	t.Run("Multiplication: Franc", func(t *testing.T) {
-		f := NewFranc(5)
-		assert.Equal(t, NewFranc(10), f.Times(2))
-		assert.Equal(t, NewFranc(15), f.Times(3))
 	})
 }
 
