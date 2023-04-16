73c3ba33913e2ef6826566a0f80ec3fe2bf1630d
yoiki <ishihara_yoshiaki@cyberagent.co.jp>
2023-04-16 17:55:22 +0900

    Add reduce function to exchange for cash

--- pkg/model/bank.go
+++ pkg/model/bank.go
@@ -3,18 +3,10 @@ package model
 type Bank struct {
 }
 
-func NewBank() (Bank, error) {
-	b := Bank{}
-	if err := b.validation(); err != nil {
-		return Bank{}, err
-	}
-	return Bank{}, nil
-}
-
-func (b Bank) validation() error {
-	return nil
+func NewBank() Bank {
+	return Bank{}
 }
 
 func (b Bank) Reduce(expression Expression) Money {
-	return Money{10, MonetaryUnitUSD}
+	return expression.Reduce()
 }
--- pkg/model/money_test.go
+++ pkg/model/money_test.go
@@ -9,42 +9,47 @@ import (
 func TestMoney_NewMoney(t *testing.T) {
 	t.Parallel()
 	t.Run("New money: Dollar", func(t *testing.T) {
-		got, err := NewMoney(5, MonetaryUnitUSD)
-		assert.Equal(t, got, Money{5, MonetaryUnitUSD})
-		assert.NoError(t, err)
+		got := NewMoney(5, MonetaryUnitUSD)
+		assert.Equal(t, got, Money{amount: 5, monetaryUnit: MonetaryUnitUSD})
 	})
 	t.Run("New money: Franc", func(t *testing.T) {
-		got, err := NewMoney(5, MonetaryUnitCHF)
-		assert.Equal(t, got, Money{5, MonetaryUnitCHF})
-		assert.NoError(t, err)
-	})
-	t.Run("Invalid money", func(t *testing.T) {
-		got, err := NewMoney(5, MonetaryUnitUnknown)
-		assert.Equal(t, got, Money{})
-		assert.Error(t, err)
+		got := NewMoney(5, MonetaryUnitCHF)
+		assert.Equal(t, got, Money{amount: 5, monetaryUnit: MonetaryUnitCHF})
 	})
 }
 
 func TestMoney_Getter(t *testing.T) {
 	t.Parallel()
 	t.Run("Amount", func(t *testing.T) {
-		m := Money{10, MonetaryUnitUSD}
+		m := Money{amount: 10, monetaryUnit: MonetaryUnitUSD}
 		assert.Equal(t, 10, m.Amount())
 		assert.NotEqual(t, 5, m.Amount())
 	})
 	t.Run("MonetaryUnit", func(t *testing.T) {
-		m := Money{10, MonetaryUnitUSD}
+		m := Money{amount: 10, monetaryUnit: MonetaryUnitUSD}
 		assert.Equal(t, MonetaryUnitUSD, m.MonetaryUnit())
 		assert.NotEqual(t, MonetaryUnitCHF, m.MonetaryUnit())
+	})
+}
+
+func TestMoney_Reduce(t *testing.T) {
+	t.Parallel()
+	t.Run("Reduce money", func(t *testing.T) {
+		m := Money{4, MonetaryUnitUSD}
+
+		b := NewBank()
+		got := b.Reduce(m)
+
+		assert.Equal(t, m, got)
 	})
 }
 
 func TestMoney_Multiplication(t *testing.T) {
 	t.Parallel()
 	t.Run("Multiplication", func(t *testing.T) {
-		m := Money{5, MonetaryUnitUSD}
-		assert.Equal(t, Money{10, MonetaryUnitUSD}, m.Times(2))
-		assert.Equal(t, Money{15, MonetaryUnitUSD}, m.Times(3))
+		m := Money{amount: 5, monetaryUnit: MonetaryUnitUSD}
+		assert.Equal(t, Money{amount: 10, monetaryUnit: MonetaryUnitUSD}, m.Times(2))
+		assert.Equal(t, Money{amount: 15, monetaryUnit: MonetaryUnitUSD}, m.Times(3))
 	})
 }
 
@@ -58,20 +63,15 @@ func TestMoney_Equals(t *testing.T) {
 	})
 }
 
-func TestMoney_Plus(t *testing.T) {
+func TestSum_Reduce(t *testing.T) {
 	t.Parallel()
-	t.Run("Simple Addition", func(t *testing.T) {
-		b, err := NewBank()
-		if err != nil {
-			t.Fatal(err)
-		}
-		m := Money{5, MonetaryUnitUSD}
-		sum := m.Plus(m)
+	t.Run("Reduce sum", func(t *testing.T) {
+		m := Money{amount: 5, monetaryUnit: MonetaryUnitUSD}
+		sum := m.Plus(m, MonetaryUnitUSD)
+
+		b := NewBank()
 		got := b.Reduce(sum)
-		assert.Equal(t, Money{10, MonetaryUnitUSD}, got)
-		//
-		//sum = m.Plus(Money{15, MonetaryUnitUSD})
-		//got = b.Reduce(sum)
-		//assert.Equal(t, Money{20, MonetaryUnitUSD}, got)
+
+		assert.Equal(t, Money{amount: 10, monetaryUnit: MonetaryUnitUSD}, got)
 	})
 }
